2ec3ceb204d2b4dd9f1ae3e8313a42f9e23766d3
engincan17 <74155046+engincan17@users.noreply.github.com>
2022-03-10 19:25:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-        TOKEN: '', //write your discord bot token
+        TOKEN: 'OTUxNTExNzQ3MDE2MTM0NzM3.YioiXg.qPwtQay67FVkFI8oUECpVsiCtDQ', //write your discord bot token
         px: '!',
-        playing: 'Umut Bayraktar ❤️ Music Bot',
+        playing: 'Hamsi V2',
 
     opt: {
         DJ: {
